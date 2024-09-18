0a20b7589406f1fd4b7bfe49a7ed9bb52269a46f
citydroid <jirafanet@gmail.com>
2024-09-19 00:50:30 +0200

    Game Manager updates

--- BigFish/Assets/Scripts/FishSpawner.cs
+++ BigFish/Assets/Scripts/FishSpawner.cs
@@ -10,9 +10,11 @@ public class FishSpawner : MonoBehaviour
     [SerializeField] private GameObject fishPrefab;
     [SerializeField] private float height;
     [SerializeField] private int spawnCount = 0;
+    [SerializeField] private bool isWhite = false;
 
     private float _timer = 0f;
     private GameObject _baseObject;
+
     void Start()
     {
         _baseObject = GameObject.Find("Base");
@@ -37,7 +39,21 @@ public class FishSpawner : MonoBehaviour
 
             GameObject newFish = Instantiate(fishPrefab, position, Quaternion.identity);
             newFish.transform.SetParent(_baseObject.transform, false);
+
+            if (isWhite)
+            {
+                Transform textTransform = newFish.transform.Find("Red");
+                GameObject valueObject = textTransform.gameObject;
+                if (valueObject != null)
+                {
+                    valueObject.SetActive(false);
+                }
+            }
         }
+    }
+    public void IsWhiteSwitch()
+    {
+        isWhite = true;
     }
     public void IncreaseSpawnRate(float newMaxTime, int newSpawnCount)
     {
--- BigFish/Assets/Scripts/GameManager.cs
+++ BigFish/Assets/Scripts/GameManager.cs
@@ -17,27 +17,52 @@ public class GameManager : MonoBehaviour
     [SerializeField] private FishSpawner fishSpawner_2;
  */
     [SerializeField] private int _level = 1; 
-    [SerializeField] private int scoreThreshold_1 = 10;
-    [SerializeField] private int scoreThreshold_2 = 20;
-    [SerializeField] private int scoreThreshold_3 = 40;
+    [SerializeField] private int[] scoreThreshold = new int[]
+    {
+        0,
+        10,
+        50,
+        100
+    };
 
-    private bool[] hasIncreasedSpawn =  new bool[] { false, false, false, false, false };
+    private readonly bool[] isWhiteDo = new bool[] { false, false, false, false };
+    private readonly bool[] hasIncreasedSpawn =  new bool[] { false, false, false, false };
+
+    private bool _isSceneChanging = false;
     public void Start()
     {
         Time.timeScale = 1.0f;
         transform.parent = null;
+        // Подписываемся на событие загрузки новой сцены
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Сбрасываем флаг при загрузке новой сцены
+        _isSceneChanging = false;
     }
     private void Update()
     {
+        if (_isSceneChanging)
+        {
+            return;  // Останавливаем выполнение, если сцена меняется
+        }
+
         int currentScore = Score.instance.GetScore();
 
-        if (currentScore >= scoreThreshold_1 && !hasIncreasedSpawn[1])
+        IsWhiteChanger(currentScore);
+
+        if (currentScore >= scoreThreshold[1] && !hasIncreasedSpawn[1])
         {
             Level_1();
         }
-        if (currentScore >= scoreThreshold_2 && !hasIncreasedSpawn[2])
+        if (currentScore >= scoreThreshold[2] && !hasIncreasedSpawn[2])
         {
             Level_2();
+        }
+        if (currentScore >= scoreThreshold[3] && !hasIncreasedSpawn[3])
+        {
+            Level_3();
         }
     }
     public void GameOver()
@@ -58,6 +83,18 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
+    private void IsWhiteChanger(int _score)
+    {
+        if (_score >= 10 && !isWhiteDo[2]) {
+            fishSpawner_10.IsWhiteSwitch();
+            isWhiteDo[2] = true;
+        }
+        else if (_score >= 50 && !isWhiteDo[3]) {
+            fishSpawner_50.IsWhiteSwitch();
+            isWhiteDo[3] = true;
+        }
+    }
+
     private void Level_1()
     {
         // Увеличиваем количество спавнящихся объектов
@@ -66,12 +103,14 @@ public class GameManager : MonoBehaviour
     }
     private void Level_2()
     {
+        fishSpawner_2.IncreaseSpawnRate(3f, 1);
         // Увеличиваем количество спавнящихся объектов
-        fishSpawner_10.IncreaseSpawnRate(0.5f, 3);  // Уменьшаем время спавна и увеличиваем количество объектов
+        fishSpawner_10.IncreaseSpawnRate(1f, 3);  // Уменьшаем время спавна и увеличиваем количество объектов
         hasIncreasedSpawn[2] = true;  // Ставим флаг, чтобы увеличить спавн только один раз
     }
     private void Level_3()
     {
+        fishSpawner_10.IncreaseSpawnRate(2f, 2);
         // Увеличиваем количество спавнящихся объектов
         fishSpawner_50.IncreaseSpawnRate(0.5f, 3);  // Уменьшаем время спавна и увеличиваем количество объектов
         hasIncreasedSpawn[3] = true;  // Ставим флаг, чтобы увеличить спавн только один раз
